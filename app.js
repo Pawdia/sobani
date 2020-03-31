feebc00a8c2eff083ca6b8c857c923280d8b213f
Ayaka Neko <neko@ayaka.moe>
2020-03-31 23:21:52 +0800

    Disabled dev tools
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ function createWindow() {
     window.loadFile("index.html")
 
     // Open DevTools
-    window.webContents.openDevTools()
+    // window.webContents.openDevTools()
 }
 
 let TrayMenu = [
