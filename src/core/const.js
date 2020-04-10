44f1b7d557c03c726577ef57851e33bc2f48c870
Cocoa <0xbbc@0xbbc.com>
2020-04-11 06:01:07 +0800

    Added constant variables into  to avoid possible typos
    Signed-off-by: Cocoa <0xbbc@0xbbc.com>

---
+++ src/core/const.js
@@ -0,0 +1,25 @@
+const __AnnounceAction = "announce"
+const __AnnouncedAction = "announced"
+const __AliveAction = "alive"
+const __AlivedAction = "alived"
+const __AudiobufPrefix = "audiobuf"
+const __PushedAction = "pushed"
+const __KnockAction = "knock"
+const __IncomeAction = "income"
+const __ChatAction = "chat"
+const __AnswerAction = "answer"
+const __DisconnectAction = "disconnect"
+
+module.exports = {
+    __AnnounceAction,
+    __AnnouncedAction,
+    __AliveAction,
+    __AlivedAction,
+    __AudiobufPrefix,
+    __PushedAction,
+    __KnockAction,
+    __IncomeAction,
+    __ChatAction,
+    __AnswerAction,
+    __DisconnectAction
+}
